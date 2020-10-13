9c5e1b48186334a2204df8dfdf5221a1e6b52275
Kimberly Suares <kimberlyjsuares@gmail.com>
2020-10-12 22:30:51 -0400

    added new html page and pseudocode for js page

---
+++ assets/JavaScript/importantlist.js
@@ -0,0 +1,11 @@
+// once  the users clickes submit  a new page opens
+
+// The new page has a textarea, where users create a new lists
+
+// Once the user clicks submit, the new list is added to original page 
+
+// All former items on the lists disappear, when the new items of the lists are added. 
+
+// In order to do this I will need to create an onclick function for when the user first clicks submit
+
+// 
